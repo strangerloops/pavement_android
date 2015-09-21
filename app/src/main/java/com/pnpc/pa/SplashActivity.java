cfb4589001af9b0d268d3660e92f1a6e5e103ae5
Markus McGee <developer@plugandplaycoding.com>
2015-09-21 13:44:53 -0500

    Update - Main screen layout update.

---
+++ app/src/main/java/com/pnpc/pa/SplashActivity.java
@@ -0,0 +1,24 @@
+package com.pnpc.pa;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by markusmcgee on 9/21/15.
+ */
+public class SplashActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+}
